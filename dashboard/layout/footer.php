a08a25ac60aee7efbff59f970808147e1cfe38e4
Jefferson Rios <jeff_rios2002@outlook.com>
2022-10-20 09:37:13 -0500

    Se ha cambiado de ubicacion el footer, header y el menu, para que se acceda desde el layout

---
+++ dashboard/layout/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <div class="footer footer-content-wrapper">
+        <div class="footer-col leftFooter m-auto small-50 tiny-100">
+            <a class="navbar-brand" href="../"><img src="../img/ico/texto.svg" style="width: 8rem; filter: invert();"></a>
+        </div>
+
+        <div class="footer-col centerFooter m-auto small-50 tiny-100">
+            <a href="https://www.facebook.com/TecnoUniISMAC" target="_blank"><i class="fab fa-facebook"></i></a>
+            <a href="www.instagram.com/institutouniversitarioismac" target="_blank"><i class="fab fa-instagram"></i></a>
+            <a href="https://github.com/jeffmermelada" target="_blank"><i class="fab fa-github"></i></a>
+        </div>
+
+        <div class="footer-col rightFooter m-auto small-50 tiny-100">
+            <ul>
+                <li><strong>&copy; Desarrollado por:</strong></li>
+                <li>Jefferson Rios</li>
+                <li>Jeremy Castro</li>
+            </ul>
+        </div>
+    </div>
+</footer>
---
+++ dashboard/layout/menu.php
@@ -0,0 +1,44 @@
+<ul class="sidebar accordion menu" id="accordionSidebar">
+            <div id="wrapper">
+                <!-- Sidebar -->
+                <nav class="main-menu">
+                    <ul>
+                        <li>
+                            <a href="./">
+                                <i class="fa fa-home"></i>
+                                <span class="nav-text">
+                                    Inicio
+                                </span>
+                            </a>
+
+                        </li>
+                        <li class="has-subnav s-active">
+                            <a href="#">
+                                <i class="fa fa-money s-active"></i>
+                                <span class="nav-text s-active">
+                                    Administrar precios
+                                </span>
+                            </a>
+
+                        </li>
+                        <li class="has-subnav">
+                            <a href="#">
+                                <i class="fa fa-bar-chart-o fa-2x"></i>
+                                <span class="nav-text">
+                                    Estadisticas
+                                </span>
+                            </a>
+
+                        </li>
+                        <li>
+                            <a href="#">
+                                <i class="fa fa-users"></i>
+                                <span class="nav-text">
+                                    Usuarios
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </ul>
