6a11fe98a5c8fde4ca39a719c97290eb1991bd9a
Jeff Rios <jeff_rios2002@outlook.com>
2023-03-22 20:33:29 -0500

    Se inicia el desarrollo para las pestañas de estadisticas

---
+++ dashboard/js/estadisticas.js
@@ -0,0 +1,82 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+const users = document.getElementById('usersArea');
+const materiales = document.getElementById('materialesPie');
+
+$.ajax({
+    url: '../php/materiales.php',
+    type: 'GET',
+    success: function (response) {
+       // console.log(response);
+    }
+})
+
+
+
+
+
+//Grafico de Barras - Usuarios
+const barras = new Chart(users, {
+    type: 'bar',
+  data: {
+    labels: meses,
+    datasets: [{
+      label: "Revenue",
+      backgroundColor: "#4e73df",
+      hoverBackgroundColor: "#2e59d9",
+      borderColor: "#4e73df",
+      data: [4215, 5312, 6251, 7841, 9821, 14984],
+    }],
+  },
+  options: {
+    maintainAspectRatio: false,
+    layout: {
+      padding: {
+        left: 10,
+        right: 25,
+        top: 25,
+        bottom: 0
+      }
+    },
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'month'
+        },
+        gridLines: {
+          display: false,
+          drawBorder: false
+        },
+        ticks: {
+          maxTicksLimit: 6
+        },
+        maxBarThickness: 25,
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 15000,
+          maxTicksLimit: 5,
+          padding: 10,
+          // Include a dollar sign in the ticks
+          callback: function(value, index, values) {
+            return '$' + number_format(value);
+          }
+        },
+        gridLines: {
+          color: "rgb(234, 236, 244)",
+          zeroLineColor: "rgb(234, 236, 244)",
+          drawBorder: false,
+          borderDash: [2],
+          zeroLineBorderDash: [2]
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
+
+//Grafico de Pie - Materiales
+const pie = new Chart(materiales, {
+})
