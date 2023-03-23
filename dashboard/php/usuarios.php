e5305d01e9e0306c8f33579380321b2ded2e3156
Jeff Rios <jeff_rios2002@outlook.com>
2023-03-22 20:39:43 -0500

    CRUD y consultas de php y MySQL

--- dashboard/php/usuarios.php
+++ dashboard/php/usuarios.php
@@ -2,11 +2,82 @@
 
 include '../../model/conexion_database.php';
 
-    $select = "SELECT * FROM `usuario`";
+if(isset($_POST["opcion"]))
+{
+    $opcion = $_POST["opcion"];
+    switch($opcion){
+        case "insertar":
+            extract($_POST);
+            if(isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["cedula"])){
 
-    $resultado = mysqli_query($con, $select);
-    while($row=mysqli_fetch_assoc($resultado)){
-        $data ["data"][] = $row;
+                $nombre = $_POST["nombre"];
+                $apellido = $_POST["apellido"];
+                $cedula = $_POST["cedula"];
+                $correo = "";
+                $direccion = "";
+                $material = "";
+                $peso = "";
+                $precio = "";
+
+                if(isset($_POST["correo"])) $correo = $_POST["correo"] ;
+                if(isset($_POST["direccion"])) $direccion = $_POST["direccion"] ;
+                if(isset($_POST["material"])) $material = $_POST["material"] ;
+                if(isset($_POST["peso"])) $peso = $_POST["peso"] ;
+                if(isset($_POST["precio"])) $precio = $_POST["precio"] ;   
+            }
+            $sql = "INSERT INTO `usuario`(`id`, `cedula`, `nombre`, `apellido`, `correo`, `direccion`, `material`, `peso`, `precio`) 
+            VALUES ('','$cedula','$nombre','$apellido','$correo','$direccion','$material','$peso','$precio')";
+            $result = mysqli_query($con, $sql);
+            break;
+
+        case "eliminar":
+            $eliminar = $_POST["idUsuario"];
+            $sql = "DELETE FROM `usuario` WHERE `id`='$eliminar'";
+            $resultado = mysqli_query($con, $sql);
+            break;
+
+        case "leer":
+            $id = $_POST["id"];
+            $sql = "SELECT user.id, user.cedula, user.nombre, user.apellido, user.correo, user.direccion, user.material as id_material, (SELECT material FROM precio_materiales WHERE precio_materiales.id = user.material) 
+            as materiales, user.peso, user.precio FROM `usuario` as user where user.id = $id;";
+            $result = mysqli_query($con, $sql);
+            $row = mysqli_fetch_array($result);          
+            echo json_encode($row);
+            break;
+
+        case "actualizar":
+            if(isset($_POST["nombre"]) && isset($_POST["apellido"]) && isset($_POST["cedula"]) && isset($_POST["id"])){
+                $id = $_POST["id"];
+                $nombre = $_POST["nombre"];
+                $apellido = $_POST["apellido"];
+                $cedula = $_POST["cedula"];
+                $correo = "";
+                $direccion = "";
+                $material = "";
+                $peso = "";
+                $precio = "";
+
+                if(isset($_POST["correo"])) $correo = $_POST["correo"] ;
+                if(isset($_POST["direccion"])) $direccion = $_POST["direccion"] ;
+                if(isset($_POST["material"])) $material = $_POST["material"] ;
+                if(isset($_POST["peso"])) $peso = $_POST["peso"] ;
+                if(isset($_POST["precio"])) $precio = $_POST["precio"] ;   
+            }
+            $sql = "UPDATE `usuario` 
+            SET `cedula`='$cedula',
+            `nombre`='$nombre',
+            `apellido`='$apellido',
+            `correo`='$correo',
+            `direccion`='$direccion',
+            `material`='$material',
+            `peso`='$peso',
+            `precio`='$precio' WHERE `id`= $id;";
+            $result = mysqli_query($con, $sql);
+            break;    
+
+        default:
+            echo "No se ha selecionado ninguna opcion";
+            break;    
     }
-    echo json_encode($data);
     
+}
