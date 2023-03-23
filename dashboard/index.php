2aafed943c242701c38b1c697d26ec6e1b683422
Jeff Rios <jeff_rios2002@outlook.com>
2023-03-22 20:33:14 -0500

    Se ha cambiado de titulo a una tarjeta

--- dashboard/index.php
+++ dashboard/index.php
@@ -331,9 +331,9 @@ if (empty($_SESSION['id'])) {
                             <div class="card shadow mb-4">
                                 <!-- Card Header - Dropdown -->
                                 <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                                    <h6 class="m-0 font-weight-bold text-primary">Número de usuarios mensuales</h6>
                                     <div class="dropdown no-arrow">
-                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                             <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
                                         </a>
                                         <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
@@ -348,7 +348,7 @@ if (empty($_SESSION['id'])) {
                                 <!-- Card Body -->
                                 <div class="card-body">
                                     <div class="chart-area">
-                                        <canvas id="myAreaChart"></canvas>
+                                        <canvas id="usersArea"></canvas>
                                     </div>
                                 </div>
                             </div>
@@ -376,7 +376,7 @@ if (empty($_SESSION['id'])) {
                                 <!-- Card Body -->
                                 <div class="card-body">
                                     <div class="chart-pie pt-4 pb-2">
-                                        <canvas id="myPieChart"></canvas>
+                                        <canvas id="materialesPie"></canvas>
                                     </div>
                                     <div class="mt-4 text-center small">
                                         <span class="mr-2">
@@ -610,6 +610,7 @@ if (empty($_SESSION['id'])) {
     <!-- Page level custom scripts -->
     <script src="../js/demo/chart-area-demo.js"></script>
     <script src="../js/demo/chart-pie-demo.js"></script>
+    <script src="./js/estadisticas.js"></script>
 
     <!-- Tarjetas -->
     <script src="./js/tarjetas.js"></script>
