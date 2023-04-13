9450d76f21559ae8b7bfdb972177fab6ee3c72f7
Jeff Rios <jeff_rios2002@outlook.com>
2023-04-13 08:18:19 -0500

    Se ha reducido el tiempo de actualización

--- js/actualizar.js
+++ js/actualizar.js
@@ -2,11 +2,9 @@ function tiempoReal() {
     var tabla = $.ajax({
         url: './php/actualizar_valor.php',
         async: false
-    }).responseText;
-    
-    document.getElementById("valorA").innerHTML = tabla;
-    
+    }).responseText;   
+    document.getElementById("valorA").innerHTML = tabla;  
 }
-setInterval(tiempoReal, 1000);
+setInterval(tiempoReal, 500);
 
 
