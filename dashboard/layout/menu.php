3efe83a8561adc09a301a92c9b899254e4a40838
Jeff Rios <jeff_rios2002@outlook.com>
2023-04-17 13:26:50 -0500

    Se modifica para agregar el nombre de la empresa

--- dashboard/layout/menu.php
+++ dashboard/layout/menu.php
@@ -13,15 +13,6 @@
 
                 </li>
                 <li>
-                    <a href="./precios_crud.php">
-                        <i class="fa fa-money"></i>
-                        <span class="nav-text">
-                            Administrar precios
-                        </span>
-                    </a>
-
-                </li>
-                <li>
                     <a href="./estadisticas.php">
                         <i class="fa fa-bar-chart-o fa-2x"></i>
                         <span class="nav-text">
@@ -31,10 +22,35 @@
 
                 </li>
                 <li>
+                    <a href="./precios_crud.php">
+                        <i class="fa fa-money"></i>
+                        <span class="nav-text">
+                            Administrar precios
+                        </span>
+                    </a>
+
+                </li>
+                <li>
                     <a href="./administrar_usuarios.php">
+                        <i class="fa fa-user"></i>
+                        <span class="nav-text">
+                            Adminsitrar clientes
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="./roles.php">
                         <i class="fa fa-users"></i>
                         <span class="nav-text">
-                            Usuarios
+                            Roles de usuario
+                        </span>
+                    </a>
+                </li>
+                <li>
+                    <a href="./perfil.php">
+                        <i class="fa fa-cog"></i>
+                        <span class="nav-text">
+                            Configurar perfil
                         </span>
                     </a>
                 </li>
--- layout/footer.php
+++ layout/footer.php
@@ -1,21 +1,36 @@
 <footer>
     <div class="footer footer-content-wrapper">
-        <div class="footer-col leftFooter m-auto small-50 tiny-100">
-            <a class="navbar-brand" href="./"><img src="./img/ico/texto.svg" style="width: 8rem; filter: invert();"></a>
+        <div class="footer-col leftFooter col-sm-5 small-50 tiny-100">
+            <div class="text">
+                <p class="fw-bold fs-4">Acerca de: </p>
+            </div>
+            
+            <a class="navbar-brand ms-1" href="./"><img src="./img/ico/texto.svg" style="width: 8rem; filter: invert();"></a>
+            <p class="text-left mt-3 p-1">
+                <strong> "Báscula All In One" </strong>es un proyecto para incentivar a las personas a reciclar, 
+                esto con el fin de ayudar a nuestro planeta.
+            </p>
         </div>
 
-        <div class="footer-col centerFooter m-auto small-50 tiny-100">
-            <a href="https://www.facebook.com/TecnoUniISMAC" target="_blank"><i class="fab fa-facebook"></i></a>
-            <a href="www.instagram.com/institutouniversitarioismac" target="_blank"><i class="fab fa-instagram"></i></a>
-            <a href="https://github.com/jeffmermelada" target="_blank"><i class="fab fa-github"></i></a>
-        </div>
-
-        <div class="footer-col rightFooter m-auto small-50 tiny-100">
+        <div class="footer-col col-sm-3 centerFooter small-50 tiny-100">
+            <div class="text">
+                <p class="fw-bold fs-4">Informacion de contacto: </p>
+            </div>
             <ul>
-                <li><strong>&copy; Desarrollado por:</strong></li>
-                <li>Jefferson Rios</li>
-                <li>Jeremy Castro</li>
+            <li><i class="fa fa-map-marker" style="font-size: 1rem;"></i><strong>Dirección: </strong>Belermo S2-02 Y, Quito 170184</li>
+            <li><i class="fa fa-globe" style="font-size: 1rem;"></i><strong>Web Site: </strong>https://ismac.edu.ec/</li>
+            <li><i class="fa fa-phone" style="font-size: 1rem;"></i><strong>Teléfono: </strong>(02) 237-8606</li>
             </ul>
         </div>
+        <div class="footer-col centerFooter m-auto small-50 tiny-100">
+            <div class="tex">
+                <p class="m-0 fs-4 fw-bold">Desarrollado por: </p>
+            </div>
+            <img src="./img/ico/pachatec/logo horizontal.svg" alt="Logo de PachaTec" style="width: 15rem;">
+        </div>
+
+    </div>
+    <div class="container m-0 p-2 pb-3 border-top">
+        <p class="m-0 p-0"> Copyright &copy; 2023 - Desarrollado con ❤ por PachaTec  </p>
     </div>
 </footer>
